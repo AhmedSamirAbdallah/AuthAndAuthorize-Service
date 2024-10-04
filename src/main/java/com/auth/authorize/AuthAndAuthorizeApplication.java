21c978a4f8572d65e4eda92fc9a01624b4a10990
Ahmed Samir <ahmedsamir661998@gmail.com>
2024-10-04 14:40:32 +0300

    [add] dependancy

---
+++ src/main/java/com/auth/authorize/AuthAndAuthorizeApplication.java
@@ -0,0 +1,13 @@
+package com.auth.authorize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthAndAuthorizeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthAndAuthorizeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/auth/authorize/AuthAndAuthorizeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auth.authorize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthAndAuthorizeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
